5e0141cb9afcfb6b9f615ed6d3506fc98a01fd93
Agustin Tabarcache <agustintabarcache74@gmail.com>
2026-06-25 16:50:13 -0300

    feat: create ComunityCard

---
+++ components/ui/cards/CommunityCard.tsx
@@ -0,0 +1,39 @@
+import type { FC } from 'react'
+import { IoChatbubbleEllipsesOutline } from 'react-icons/io5'
+
+import Button from '../Button'
+
+const CommunityCard: FC = () => {
+  return (
+    <article
+      className="
+        max-w-125
+        max-h-70
+        py-9
+        px-8
+        rounded
+        bg-black/65
+        flex
+        flex-col
+        items-center
+      "
+    >
+      <h2 className="text-4xl text-gold text-center">
+        Únete a nuestra comunidad
+      </h2>
+
+      <p className="text-base text-cream text-center my-4">
+        Habla y comparte con más personas con tus mismos gustos
+      </p>
+
+      <Button
+        variant="secondary"
+        label="Unirse a la comunidad"
+        logo={<IoChatbubbleEllipsesOutline aria-hidden="true" focusable="false" />}
+        aria-label="Unirse a la comunidad"
+      />
+    </article>
+  )
+}
+
+export default CommunityCard
